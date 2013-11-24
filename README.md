ce232ed98abe6fd56919530636812397f99756af
Jared Hanson <jaredhanson@gmail.com>
2013-11-24 08:48:00 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# chai-locomotive-helpers
---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('chai-locomotive-helpers', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
