f77d5c088694f8857e4f3e41563455e68e669757
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 12:48:24 -0800

    Test case for Test#req.

---
+++ test/test.req.test.js
@@ -0,0 +1,64 @@
+/* global describe, it, before, expect */
+
+var Test = require('../lib/test');
+
+describe('test dynamic helper that prepares request', function() {
+  
+  function dynamicHelper(req, res) {
+    return function() {
+      return 'Help me, ' + req.query.name;
+    }
+  }
+  
+  describe('sync', function() {
+    
+    describe('and creates', function() {
+      var help;
+    
+      before(function(done) {
+        var test = new Test(dynamicHelper, 'test', 'show', true);
+        test.req(function(req) {
+          req.query = {};
+          req.query.name = 'Rhonda';
+        }).create(function(err, h) {
+          if (err) { return done(err); }
+          help = h;
+          done();
+        });
+      });
+    
+      it('should create helper', function() {
+        expect(help).to.be.a('function');
+        expect(help()).to.equal('Help me, Rhonda');
+      });
+    });
+    
+  });
+  
+  describe('async', function() {
+    
+    describe('and creates', function() {
+      var res;
+    
+      before(function(done) {
+        var test = new Test(dynamicHelper, 'test', 'show', true);
+        test.req(function(req, done) {
+          req.query = {};
+          req.query.name = 'asynchronously, Rhonda';
+          process.nextTick(done);
+        }).create(function(err, h) {
+          if (err) { return done(err); }
+          help = h;
+          done();
+        });
+      });
+    
+      it('should create helper', function() {
+        expect(help).to.be.a('function');
+        expect(help()).to.equal('Help me, asynchronously, Rhonda');
+      });
+    });
+    
+  });
+  
+});
