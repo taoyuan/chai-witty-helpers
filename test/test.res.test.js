cc98f557946bfb54ab13e8ec11e175dba52000fa
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 15:58:42 -0800

    Test case for Test#res.

---
+++ test/test.res.test.js
@@ -0,0 +1,38 @@
+/* global describe, it, before, expect */
+
+var Test = require('../lib/test');
+
+describe('test dynamic helper that prepares response', function() {
+  
+  function dynamicHelper(req, res) {
+    return function() {
+      return 'Help me, ' + res.locals.name;
+    }
+  }
+  
+  describe('sync', function() {
+    
+    describe('and creates', function() {
+      var pres, eres;
+    
+      before(function(done) {
+        var test = new Test(dynamicHelper, 'test', 'show', true);
+        test.res(function(res) {
+          res.locals = {};
+          res.locals.name = 'I think I\"m falling';
+        }).create(function(err, h) {
+          if (err) { return done(err); }
+          help = h;
+          done();
+        });
+      });
+    
+      it('should create helper', function() {
+        expect(help).to.be.a('function');
+        expect(help()).to.equal('Help me, I think I\"m falling');
+      });
+    });
+    
+  });
+  
+});
