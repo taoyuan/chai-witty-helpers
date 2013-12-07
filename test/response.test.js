6754fbf09c795b694a54966d3c184e1925642b45
Jared Hanson <jaredhanson@gmail.com>
2013-12-07 12:36:31 -0800

    Test case for Response.

---
+++ test/response.test.js
@@ -0,0 +1,25 @@
+/* global describe, it, before, expect */
+
+var Response = require('../lib/response');
+
+describe('Response', function() {
+  
+  describe('constructor', function() {
+    var res = new Response();
+  
+    it('should be constructed with default properties', function() {
+      expect(Object.keys(res)).to.have.length(2);
+      expect(res.statusCode).to.equal(200);
+    });
+  });
+  
+  describe('#setHeader', function() {
+    var res = new Response();
+    res.setHeader('Content-Type', 'application/json');
+  
+    it('should get set header', function() {
+      expect(res.getHeader('Content-Type')).to.equal('application/json');
+    });
+  });
+  
+});
