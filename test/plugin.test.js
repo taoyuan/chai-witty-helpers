ce84d69b3f876861d6ce3d74206b8d43eb0f2842
taoyuan <torworx@gmail.com>
2015-05-16 20:18:32 +0800

    Rename to chai-witty-helpers

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -8,14 +8,14 @@ describe('plugin', function() {
   var chai = {};
   plugin(chai);
   
-  it('should add maglev helper to chai', function() {
-    expect(chai.maglev).to.be.an('object');
-    expect(chai.maglev.helper).to.be.a('function');
-    expect(chai.maglev.dynamicHelper).to.be.a('function');
+  it('should add witty helper to chai', function() {
+    expect(chai.witty).to.be.an('object');
+    expect(chai.witty.helper).to.be.a('function');
+    expect(chai.witty.dynamicHelper).to.be.a('function');
   });
   
   describe('when helper is invoked', function() {
-    var test = chai.maglev.helper();
+    var test = chai.witty.helper();
     
     it('should return test wrapper', function() {
       expect(test).to.be.an.instanceOf(Test);
@@ -23,7 +23,7 @@ describe('plugin', function() {
   });
   
   describe('when dynamicHelper is invoked', function() {
-    var test = chai.maglev.dynamicHelper();
+    var test = chai.witty.dynamicHelper();
     
     it('should return test wrapper', function() {
       expect(test).to.be.an.instanceOf(Test);
